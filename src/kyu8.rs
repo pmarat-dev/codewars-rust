51741fca8099373bfc00b9be918b360cbf721cb6
Piotr <pmarat.dev@gmail.com>
2023-11-28 17:12:31 +0100

    Added total_amount_of_points.rs.

--- src/kyu8.rs
+++ src/kyu8.rs
@@ -1,3 +1,4 @@
 pub mod string_to_number;
 pub mod number_to_string;
 pub mod add_length;
+pub mod total_amount_of_points;
---
+++ src/kyu8/total_amount_of_points.rs
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5bb904724c47249b10000131
+// Total amount of points
+
+use std::cmp::Ordering;
+
+#[allow(dead_code)]
+fn total_amount_of_points(games: &[&str]) -> u32 {
+    games.iter().map(|game| {
+        let (scored, conceded) = game.split_once(':').unwrap();
+        match scored.cmp(conceded) {
+            Ordering::Less => 0,
+            Ordering::Equal => 1,
+            Ordering::Greater => 3,
+        }
+    }).sum()
+}
+
+#[allow(dead_code)]
+pub fn total_amount_of_points_test() {
+    println!("{}", total_amount_of_points(&["1:0", "0:0", "0:2", "3:1"]));
+}
