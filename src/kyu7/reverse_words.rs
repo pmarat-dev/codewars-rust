5ca7a50af48b7df224d6d5142042ee25f6f801e8
Piotr <pmarat.dev@gmail.com>
2023-12-04 16:53:44 +0100

    Added reverse_words.rs.

---
+++ src/kyu7/reverse_words.rs
@@ -0,0 +1,11 @@
+
+fn reverse_words(str: &str) -> String {
+    str.to_string().split(" ")
+        .map(|word| word.chars().rev().collect::<String>())
+        .collect::<Vec<String>>().join(" ")
+}
+
+#[allow(dead_code)]
+pub fn reverse_words_test() {
+    println!("{}", reverse_words("Hello World out there!"));
+}
